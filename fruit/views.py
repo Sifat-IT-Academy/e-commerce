0f313c8f0919ecf8d4df461b320e15dcdbc99cf5
muslimbek77 <muslimjonbekali@mail.ru>
2024-05-18 18:06:38 +0500

    view

--- fruit/views.py
+++ fruit/views.py
@@ -8,33 +8,34 @@ from .models import Contact
 from django.views.generic import View,CreateView
 
 def home_view(request):
+    
     return render(request,"index.html")
 
 def shop_view(request):
     return render(request,"shop.html")
 
-# class ContactView(View):
-#     template_name = "contact.html"
-    
-#     def get(self, request, *args, **kwargs):
-#         return render(request, self.template_name)
-    
-#     def post(self, request, *args, **kwargs): 
-#         name = request.POST.get('first_name', '')
-#         email = request.POST.get('email', '')
-#         message = request.POST.get('description', '')
-#         contact = Contact(first_name=name,email=email,description=message)
-#         contact.save()
-        
-#         send_message(f"Ism: {name}\nEmail: {email}\nText:{message}")
-
-#         return HttpResponseRedirect(reverse('home-page'))   
-
-class ContactView(CreateView):
-    model = Contact
-    form_class = ContactForm
+class ContactView(View):
     template_name = "contact.html"
-    success_url ='/'
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs): 
+        name = request.POST.get('first_name', '')
+        email = request.POST.get('email', '')
+        message = request.POST.get('description', '')
+        contact = Contact(first_name=name,email=email,description=message)
+        contact.save()
+        
+        send_message(f"Ism: {name}\nEmail: {email}\nText:{message}")
+
+        return HttpResponseRedirect(reverse('home-page'))   
+
+# class ContactView(CreateView):
+#     model = Contact
+#     form_class = ContactForm
+#     template_name = "contact.html"
+#     success_url ='/'
 
 
 # def contact_view(request):
