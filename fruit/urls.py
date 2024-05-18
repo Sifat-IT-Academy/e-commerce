2b05b94005ab8f28841d4986ac675b300601cd63
muslimbek77 <muslimjonbekali@mail.ru>
2024-05-18 18:00:41 +0500

    class view

--- fruit/urls.py
+++ fruit/urls.py
@@ -1,9 +1,9 @@
-from .views import home_view,shop_view,contact_view
+from .views import home_view,shop_view,ContactView
 
 from django.urls import path
 
 urlpatterns = [
     path('', home_view, name="home-page"),
     path('shop/',shop_view,name="shop-page"),
-    path("contact/",contact_view,name="contact-page")
+    path("contact/",ContactView.as_view(),name="contact-page")
 ]
--- fruit/views.py
+++ fruit/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .forms import ContactForm
+from .bot import send_message
+from .models import Contact
+from django.views.generic import View,CreateView
 
 def home_view(request):
     return render(request,"index.html")
@@ -8,5 +13,41 @@ def home_view(request):
 def shop_view(request):
     return render(request,"shop.html")
 
-def contact_view(request):
-    return render(request,"contact.html")
+# class ContactView(View):
+#     template_name = "contact.html"
+    
+#     def get(self, request, *args, **kwargs):
+#         return render(request, self.template_name)
+    
+#     def post(self, request, *args, **kwargs): 
+#         name = request.POST.get('first_name', '')
+#         email = request.POST.get('email', '')
+#         message = request.POST.get('description', '')
+#         contact = Contact(first_name=name,email=email,description=message)
+#         contact.save()
+        
+#         send_message(f"Ism: {name}\nEmail: {email}\nText:{message}")
+
+#         return HttpResponseRedirect(reverse('home-page'))   
+
+class ContactView(CreateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = "contact.html"
+    success_url ='/'
+
+
+# def contact_view(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('first_name', '')
+#         email = request.POST.get('email', '')
+#         message = request.POST.get('description', '')
+#         contact = Contact(first_name=name,email=email,description=message)
+#         contact.save()
+        
+#         send_message(f"Ism: {name}\nEmail: {email}\nText:{message}")
+
+#         return render(request, 'contact.html')
+        
+#     else:
+#         return render(request, "contact.html")
