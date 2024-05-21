a8b7298e7f137405daf3bed0ae2e51639428b284
muslimbek77 <muslimjonbekali@mail.ru>
2024-05-21 19:46:34 +0500

    add search function in views.py

--- fruit/views.py
+++ fruit/views.py
@@ -4,12 +4,29 @@ from django.urls import reverse
 
 from .forms import ContactForm
 from .bot import send_message
-from .models import Contact
-from django.views.generic import View,CreateView
+from .models import Contact,Product,Category
+from django.views.generic import View,TemplateView
 
-def home_view(request):
+# def home_view(request):
     
-    return render(request,"index.html")
+#     return render(request,"index.html")
+
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self,*args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args,**kwargs)
+        context['products'] = Product.objects.all()
+        context['categories'] = Category.objects.all()
+        
+        query = self.request.GET.get('q')
+        if query:
+            context['products'] = Product.objects.filter(title__icontains=query)
+
+        return context
+
+    
+
 
 def shop_view(request):
     return render(request,"shop.html")
