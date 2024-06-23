270635e0d6c06c6ac16abc001eae6dbaa4577a24
muslimbek77 <muslimjonbekali@mail.ru>
2024-06-23 12:14:32 +0500

    add
    registration form

--- fruit/urls.py
+++ fruit/urls.py
@@ -1,4 +1,4 @@
-from .views import HomeView,ShopDetailView,ContactView,ShopView #new
+from .views import HomeView,ShopDetailView,ContactView,ShopView,cart,chackout,register #new
 
 from django.urls import path
 
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home-page"),
     path('shop/<slug:slug>/', ShopDetailView.as_view(),name="detail-page"),
     path('shop/',ShopView.as_view(), name="shop-page"),
-    path("contact/",ContactView.as_view(),name="contact-page")
+    path("contact/",ContactView.as_view(),name="contact-page"),
+    path("cart/",cart,name='cart'),
+    path("chackout/",chackout,name='chackout'),
+    path("registration/",register,name='register')
+
 
 ]
--- fruit/views.py
+++ fruit/views.py
@@ -94,24 +94,25 @@ class ContactView(View):
 
         return HttpResponseRedirect(reverse('home-page'))   
 
-# class ContactView(CreateView):
-#     model = Contact
-#     form_class = ContactForm
-#     template_name = "contact.html"
-#     success_url ='/'
+def chackout(request):
+    return render(request,"chackout.html")
 
+def cart(request):
+    return render(request,"cart.html")
 
-# def contact_view(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('first_name', '')
-#         email = request.POST.get('email', '')
-#         message = request.POST.get('description', '')
-#         contact = Contact(first_name=name,email=email,description=message)
-#         contact.save()
-        
-#         send_message(f"Ism: {name}\nEmail: {email}\nText:{message}")
+from .forms import LoginForm, UserRegistrationForm
 
-#         return render(request, 'contact.html')
-        
-#     else:
-#         return render(request, "contact.html")
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'register.html', {'user_form': user_form})
