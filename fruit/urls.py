63b647a03c02e65216c5a4bfd828861a1c6b5187
muslimbek77 <muslimjonbekali@mail.ru>
2024-06-25 16:09:00 +0500

    add product to basket

--- fruit/urls.py
+++ fruit/urls.py
@@ -1,4 +1,4 @@
-from .views import HomeView,ShopDetailView,ContactView,ShopView,cart,chackout,register #new
+from .views import HomeView,ShopDetailView,ContactView,ShopView,cart,register,add_cart,sub_cart,remove_cart #new
 
 from django.urls import path
 
@@ -7,9 +7,9 @@ urlpatterns = [
     path('shop/<slug:slug>/', ShopDetailView.as_view(),name="detail-page"),
     path('shop/',ShopView.as_view(), name="shop-page"),
     path("contact/",ContactView.as_view(),name="contact-page"),
-    path("cart/",cart,name='cart'),
-    path("chackout/",chackout,name='chackout'),
-    path("registration/",register,name='register')
-
-
+    path("registration/",register,name='register'),
+    path('cart/', cart, name="cart"),
+    path('cart/add_product/<int:product_id>/', add_cart, name="add-cart"),
+    path('cart/sub_product/<int:product_id>/', sub_cart, name="cart_sub"),
+    path('cart/remove_product/<int:product_id>/', remove_cart, name="cart_remove"),
 ]
